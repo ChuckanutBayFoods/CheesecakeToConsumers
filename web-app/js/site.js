0437d82e58498959f28b5fdb7c5683a50a96ddb9
Mitchell Loeppky <mitchell@loeppky.com>
2013-09-14 14:13:14 -0700

    Setup UI
    Included jQuery, bootstrap, and SASS

---
+++ web-app/js/site.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: mitchellloeppky
+ * Date: 9/14/13
+ * Time: 1:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+(function() {
+var test = new Date();
+    console.log(test);
+    console.log(test);
+})();
