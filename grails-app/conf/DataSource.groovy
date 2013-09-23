050d0e98bd4554163e7ed59520fe25ac48dab9e4
Steve Loeppky <stvn@loeppky.com>
2013-09-23 00:10:41 -0700

    Add -DuseProdDb for switching to use the prodDb

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,3 +1,4 @@
+
 dataSource {
 	pooled = true
 	driverClassName = "org.h2.Driver"
@@ -9,51 +10,51 @@ hibernate {
 	cache.use_query_cache = false
 	cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
-// environment specific settings
-environments {
-	development {
-		dataSource {
-			dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-			url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+def productionDataSource = {
+	// http://flnkr.com/2013/07/grails-on-aws/
+	// https://console.aws.amazon.com/elasticbeanstalk/home?region=us-east-1#/environment/configuration?applicationName=givecheesecakes.com&environmentId=e-s6cnnk2wah&edit=container
+	dataSource {
+		username = System.getProperty("JDBC_USERNAME", "jdbcUsernameNotSet")
+		password = System.getProperty("JDBC_PASSWORD", "jdbcPasswordNotSet")
+		pooled = true
+		dbCreate = "update"
+		driverClassName = "com.mysql.jdbc.Driver"
+		url = System.getProperty("JDBC_CONNECTION_STRING", "jdbcConntectionStringNotSet")
+		dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+		properties {
+			validationQuery = "SELECT 1"
+			testOnBorrow = true
+			testOnReturn = true
+			testWhileIdle = true
+			timeBetweenEvictionRunsMillis = 1000 * 60 * 30
+			numTestsPerEvictionRun = 3
+			minEvictableIdleTimeMillis = 1000 * 60 * 30
 		}
 	}
+}
+// If you use development and want to hit the production DB, you'll have to provide these VM arguments in your run configuration:
+// -DuseProdDb=true -DJDBC_CONNECTION_STRING=jdbc:foo -DJDBC_USERNAME=foo -DJDBC_PASSWORD=foo
+// (replacing "foo" with the proper value)
+// CLI:
+// - http://grails.org/doc/latest/ref/Command%20Line/run-app.html
+// - grails "-DuseProdDb=true -DJDBC_CONNECTION_STRING=foo -DJDBC_USERNAME=foo -DJDBC_PASSWORD=foo" run-app -https
+// - (the quotes are important, or you'll get param parsing issues on the command line)
+// Eclipse:
+// - http://stackoverflow.com/questions/862391/how-to-pass-the-d-sytem-properties-while-testing-on-eclipse
+def developmentDataSource = System.getProperty("useProdDb") ? productionDataSource : {
+	dataSource {
+		dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+		url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+	}
+}
+// environment specific settings
+environments {
+	production productionDataSource
+	development developmentDataSource
 	test {
 		dataSource {
 			dbCreate = "update"
 			url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-		}
-	}
-	// Use development if want to hit the prod DB from your personal machine.
-	// If you use development, you'll have to provide these VM arguments in your run configuration:
-	// -DJDBC_CONNECTION_STRING=jdbc:foo -DJDBC_USERNAME=foo -DJDBC_PASSWORD=foo
-	// (replacing "foo" with the proper value)
-	// CLI:
-	// - http://grails.org/doc/latest/ref/Command%20Line/run-app.html
-	// - grails "-DJDBC_CONNECTION_STRING=foo -DJDBC_USERNAME=foo -DJDBC_PASSWORD=foo" run-app -https
-	// - (the quotes are important, or you'll get param parsing issues on the command line)
-	// Eclipse:
-	// - http://stackoverflow.com/questions/862391/how-to-pass-the-d-sytem-properties-while-testing-on-eclipse
-	//development {
-	production {
-		// http://flnkr.com/2013/07/grails-on-aws/
-		// https://console.aws.amazon.com/elasticbeanstalk/home?region=us-east-1#/environment/configuration?applicationName=givecheesecakes.com&environmentId=e-s6cnnk2wah&edit=container
-		dataSource {
-			username = System.getProperty("JDBC_USERNAME", "jdbcUsernameNotSet")
-			password = System.getProperty("JDBC_PASSWORD", "jdbcPasswordNotSet")
-			pooled = true
-			dbCreate = "update"
-			driverClassName = "com.mysql.jdbc.Driver"
-			url = System.getProperty("JDBC_CONNECTION_STRING", "jdbcConntectionStringNotSet")
-			dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-			properties {
-				validationQuery = "SELECT 1"
-				testOnBorrow = true
-				testOnReturn = true
-				testWhileIdle = true
-				timeBetweenEvictionRunsMillis = 1000 * 60 * 30
-				numTestsPerEvictionRun = 3
-				minEvictableIdleTimeMillis = 1000 * 60 * 30
-			}
 		}
 	}
 }
