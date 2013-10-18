ae713a06a57fa48e8054b9a3752d5cde0a12a60f
Steve Loeppky <stvn@loeppky.com>
2013-10-17 22:20:11 -0700

    Modifying SendShipmentTrackingEmailJob to actually query the DB.
    EmailService now has a sendShipmentTrackingEmail method.

--- grails-app/jobs/cheesecaketoconsumers/SendShipmentTrackingEmailJob.groovy
+++ grails-app/jobs/cheesecaketoconsumers/SendShipmentTrackingEmailJob.groovy
@@ -2,13 +2,30 @@ package cheesecaketoconsumers
 
 
 
+
 class SendShipmentTrackingEmailJob {
     static triggers = {
 		// Execute job every 5 minutes indefinitely
 		simple startDelay : 60 * 1000, repeatInterval: 5 * 60 * 1000, repeatCount: -1
     }
+	
+	EmailService emailService
+	
+	// http://grails-plugins.github.io/grails-quartz/guide/configuration.html
+	def concurrent = false
 
     def execute() {
         System.out.println("Executing!!!")
+		List<Sale> sales = Sale.findAll {
+			wasShipmentTrackingEmailSent == false &&
+			shippingMethod != null &&
+			shipmentTrackingNumber != null
+		}
+		log.debug("${sales.size} sales found with shipment tracking number but no shipment tracking email sent.")
+		sales.each({
+			if (emailService.sendShipmentTrackingEmail(it)) {
+				it.wasShipmentTrackingEmailSent = true
+			}
+		})
     }
 }
--- grails-app/services/cheesecaketoconsumers/EmailService.groovy
+++ grails-app/services/cheesecaketoconsumers/EmailService.groovy
@@ -10,6 +10,7 @@ class EmailService {
 	 * All {@link Exception}s are caught and nothing is logged.
 	 */
 	boolean sendThankYouForYourOrderEmail(Sale sale) {
+		log.debug("Sending 'thanks for your order' email to ${sale.giver.emailAddress} for ${sale}")
 		try {
 			sendMail {
 				to "${sale.giver.emailAddress}"
@@ -19,7 +20,29 @@ class EmailService {
 			}
 			return true
 		} catch (Exception e) {
-			log.error("Unable to send email to ${sale.giver.emailAddress} for ${sale}", e)
+			log.error("Unable to send 'thanks for your order' email to ${sale.giver.emailAddress} for ${sale}", e)
+			return false
+		}
+	}
+	
+	/**
+	 * @param sale {@link Sale} to send a "thank you for your order" email for.
+	 * @param log {@link Log} to use for logging any errors.
+	 * @return whether the "thank you for your order email" was sent.
+	 * All {@link Exception}s are caught and nothing is logged.
+	 */
+	boolean sendShipmentTrackingEmail(Sale sale) {
+		log.debug("Sending 'shipment tracking' email to ${sale.giver.emailAddress} for ${sale}")
+		try {
+			sendMail {
+				to "${sale.giver.emailAddress}"
+				from "givecheesecakes.com <customerservice@givecheesecakes.com>"
+				subject "Your cheesecakes have shipped!"
+				text view: "/emails/thankYouForYourOrderText", model: [sale : sale, customerServiceEmailAddress : "customerservice@givecheesecakes.com"]
+			}
+			return true
+		} catch (Exception e) {
+			log.error("Unable to send 'shipment tracking' email to ${sale.giver.emailAddress} for ${sale}", e)
 			return false
 		}
 	}
