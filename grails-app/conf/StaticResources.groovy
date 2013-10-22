c467a7a59417acd3dcc0aa0800bfc228527a826d
Steve Loeppky <stvn@loeppky.com>
2013-10-22 00:44:34 -0700

    Including resources from Mitch's skrollr work.

--- grails-app/conf/StaticResources.groovy
+++ grails-app/conf/StaticResources.groovy
@@ -1,15 +1,68 @@
+import grails.util.Environment
+
+def isProd = Environment.current == Environment.PRODUCTION;
+
 modules = {
 	core {
 		defaultBundle 'common'
 		resource url: 'http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css'
 		resource url: 'css/site.css'
-		resource url: 'http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.2.1/moment.min.js'
-		resource url: 'http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js'
-		resource url: 'http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js'
-		resource url: 'http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js'
-		resource url: 'js/bootstrap-datepicker.js'
-		resource url: 'js/sly.js'
-		resource url: 'js/stripe.js'
-		resource url: 'js/site.js'
+		resource url: 'css/keyframes.css', attrs:["data-skrollr-stylesheet":""]
+		
+		// jQuery - A webapp essential
+		resource url: "https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"
+
+		// jQuery.validate - A jQuery plugin for forms validation
+		resource url: "https://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"
+
+		// moment.js - For date processing and formatting
+		resource url: "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.2.1/moment.min.js"
+
+		// bootstrap.js - UI elements such as buttons and popovers
+		resource url: "https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"
+
+		// bootstrap-datepicker.js - A datepicker UI element from bootstrap
+		resource url: "js/bootstrap-datepicker.js"
+
+		// sly.js - Carousel UI element
+		resource url: "js/sly.js"
+
+		// strip.js - Credit-card and checkout processing
+		resource url: "js/stripe.js"
+
+		// skrollr.js - Parallax and Single-Page-App framework
+		resource url: "js/skrollr.js"
+
+		// skrollr-menu.js - Navigation support for skrollr
+		resource url: "js/skrollr.menu.js"
+
+		// skrollr-stylesheets.js - CSS support for skrollr
+		resource url: "js/skrollr.stylesheets.js"
+
+		// Our own lovely js
+		resource url: "js/site.js"
 	}
+}
+
+
+// Work in progress.
+// Attempting to see if can download files locally and use them instead.
+def getUrl(String url) {
+	File tmpDir = new File(System.getProperty("java.io.tmpdir"));
+	File outputFile = new File(tmpDir, url.replaceFirst("https?://", ""));
+	println "Creating local file for ${url}.  Local file should be at ${outputFile}"
+	if (outputFile.exists()) {
+		println "Local file ${outputFile} already exists.  Returning."
+		return outputFile.path; 
+	}
+	outputFile.getParentFile().mkdirs();
+	def outputStream = new BufferedOutputStream(new FileOutputStream(outputFile))
+	outputStream << new URL(url).openStream()
+	outputStream.close()
+	
+	assert outputFile.length() > 0
+	
+	println "Created local file ${outputFile}." 
+	
+	return outputFile.path
 }
