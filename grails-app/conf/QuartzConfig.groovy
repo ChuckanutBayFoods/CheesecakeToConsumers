cf6796ddf46c54707b9ccb584406282dcc268cb7
Steve Loeppky <stvn@loeppky.com>
2013-10-21 20:35:14 -0700

    Added home/customerService URL Gating the front-end UI with requiring a param passCode=betaTest

--- grails-app/conf/QuartzConfig.groovy
+++ grails-app/conf/QuartzConfig.groovy
@@ -1,7 +1,15 @@
+
 // http://grails-plugins.github.io/grails-quartz/guide/configuration.html
 // http://docs.spring.io/spring/docs/2.5.x/api/org/springframework/scheduling/quartz/SchedulerFactoryBean.html
 // http://quartz-scheduler.org/documentation/quartz-2.2.x/configuration/ConfigMain
 quartz {
     autoStartup = true
     jdbcStore = false
+}
+environments {
+	development {
+		quartz {
+			autoStartup = false
+		}
+	}
 }
--- grails-app/controllers/cheesecaketoconsumers/HomeController.groovy
+++ grails-app/controllers/cheesecaketoconsumers/HomeController.groovy
@@ -1,12 +1,27 @@
 package cheesecaketoconsumers
 
+import grails.util.Environment
+
+
 /**
  * Controller for homepage.
  */
 class HomeController {
 	
 	def index() {
+		if (Environment.current == Environment.PRODUCTION) {
+			log.info(params)
+			if (!(params.passCode =~ "betaTest")) {
+				render("Coming soon.  Check back in 1 week.")
+				return
+			}
+		}
 		render(view: "index")
+	}
+	
+	def customerService() {
+		log.info("Redirecting to Customer Service page.")
+		redirect(url:"https://docs.google.com/document/d/1rpM6SlmaFaOuESQtR2MB_H-aOR1sp6hsS6iyd0x7UT8/edit?pli=1")
 	}
 
 }
