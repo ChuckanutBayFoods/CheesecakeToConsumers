2b87359f8ac216b17d7469c389996e9f34aa9c7d
Steve Loeppky <stvn@loeppky.com>
2013-10-25 06:40:22 -0700

    Enabling gzip compression on Elastic Beanstalk

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,4 +1,6 @@
 // Enable gzip compressions for non-static resources (e.g., /home/index)
+// This only applies for the Tomcat instance that is fired up with grails run-app.
+// One needs to follow a different set of steps for the Elastic Beanstalk Tomcat instance.
 // http://www.slideshare.net/gr8conf/gr8conf-2011-tuning-grails-applications-by-peter-ledbrook
 eventConfigureTomcat = {tomcat ->
 	tomcat.connector.setAttribute("compression", "on")
